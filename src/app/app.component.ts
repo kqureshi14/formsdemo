d8bcb4bdba6049b65b7c49dfd2280288c72280f9
Kaleem <kaleem14you@gmail.com>
2024-08-17 01:49:51 +0500

    forms demo template and reactive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ReactiveFormsModule,FormControl,FormGroup, FormsModule } from '@angular/forms';
+
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule,FormsModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'formsdemo';
+
+
+  loginForm = new FormGroup({
+   user: new FormControl(''),
+   password: new FormControl('')
+
+  });
+
+  loginUser(){
+    console.info(this.loginForm.value);
+  }
+   
+
+  userLogin(item:any){
+    console.info(item);
+
+  }
+
 }
