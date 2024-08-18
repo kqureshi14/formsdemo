905e4d9c7ee1387efd08eaf32b856548c9885268
Kaleem <kaleem14you@gmail.com>
2024-08-18 13:58:24 +0500

    added validation to reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule,FormControl,FormGroup, FormsModule, Validators} from '@angular/forms';
 
-import { ReactiveFormsModule,FormControl,FormGroup, FormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ReactiveFormsModule,FormsModule,],
+  imports: [RouterOutlet,ReactiveFormsModule,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,19 +17,29 @@ export class AppComponent {
 
 
   loginForm = new FormGroup({
-   user: new FormControl(''),
-   password: new FormControl('')
+   user: new FormControl('',[Validators.required,Validators.email]),
+   password: new FormControl('',[Validators.required,Validators.minLength(7)])
 
   });
+
+  get user(){
+    return this.loginForm.get('user');
+  }
+
+
+  get password(){
+    return this.loginForm.get('password');
+  }
+
 
   loginUser(){
     console.info(this.loginForm.value);
   }
-   
 
+
+   
   userLogin(item:any){
     console.info(item);
-
   }
 
 }
